0c1619afc32149803aed88a89a4d6562bb5abcc1
Ashpak Veetar <blackmamoth83@gmail.com>
2023-11-16 00:39:28 +0530

    Update generate_keys.py

--- prerun/generate_keys.py
+++ prerun/generate_keys.py
@@ -12,7 +12,7 @@ def generate_keys(dir_path: str):
         with open("public_key.pem", "w") as public_key_file:
             public_key_file.write(public_key.export_key())
         with open("private_key.pem", "w") as private_key_file:
-            private_key_file.write(public_key.export_key())
+            private_key_file.write(private_key.export_key())
         print(f"Private and public key successfully save at {dir_path} directory.")
     except Exception:
         print("An error occured while generating and saving keys.")
